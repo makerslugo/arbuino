04f1bf9a011e36ae260ffaeeda293d7d2f196803
Andrés Maneiro <andres.maneiro@gmail.com>
2014-11-02 16:45:45 +0100

    add test sound program

---
+++ sound/test_tone.ino
@@ -0,0 +1,36 @@
+#include "pitches.h"
+
+int melody[] = {
+  NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_G6, NOTE_C6, NOTE_D6,
+  NOTE_E6, NOTE_F6, NOTE_F6, NOTE_F6, NOTE_F6, NOTE_F6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, 
+  NOTE_D6, NOTE_D6, NOTE_E6, NOTE_D6, NOTE_G6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_E6, 
+  NOTE_E6, NOTE_E6, NOTE_G6, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_F6, NOTE_F6, NOTE_F6, NOTE_F6, 
+  NOTE_F6, NOTE_E6, NOTE_E6, NOTE_E6, NOTE_G6, NOTE_G6, NOTE_E6, NOTE_D6, NOTE_C6
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 4, 2, 4, 4, 2, 4, 4, 4, 4, 
+  1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+  4, 4, 4, 2, 2, 4, 4, 2, 4, 4, 
+  2, 4, 4, 4, 4, 1, 4, 4, 4, 4, 
+  4, 4, 4, 4, 4, 4, 4, 4, 1
+};
+
+void setup() {
+  for (int thisNote = 0; thisNote < 49; thisNote++) {
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+}
+
+void loop() {
+}
+
+
+
+
+
